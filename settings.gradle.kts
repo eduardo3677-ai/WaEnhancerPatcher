d9c415c2db02044881c82d058612362c2cf72d68
eduardo3677-ai <eduardo3677@echosmart.me>
2026-08-01 04:55:23 +0000

    fix: remove gropify, simplify to Xposed-only deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven("https://raw.githubusercontent.com/HighCapable/maven-repository/main/repository/releases")
-        maven("https://raw.githubusercontent.com/HighCapable/maven-repository/main/repository/snapshots")
     }
-}
-
-plugins {
-    id("com.highcapable.gropify") version "1.0.2" apply false
 }
 
 dependencyResolutionManagement {
@@ -18,9 +12,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://api.xposed.info/")
-        maven("https://jitpack.io")
-        maven("https://raw.githubusercontent.com/HighCapable/maven-repository/main/repository/releases")
-        maven("https://raw.githubusercontent.com/HighCapable/maven-repository/main/repository/snapshots")
     }
 }
 
